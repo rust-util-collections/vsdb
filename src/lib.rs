ce65b39f558da359de63907b8bcac4e826c9ea6e
ccmlm <fanhui.x@gmail.com>
2021-12-13 12:09:44 +0800

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 //!
-//! # vsdb
+//! # VSDB
+//!
+//! VSDB, **V**ersioned **S**tateful **D**ata**B**ase, mainly used in blockchain scene.
+//!
+//! Support some GIT-like operations, such as:
+//!
+//! - Support rolling back a 'branch' to a specified historical 'version'
+//! - Support querying the historical value of a key on the specified 'branch'
+//!
+//! All data is divided into two dimensions: 'branch' and 'version', the functions of the 'basic' category are stateless, and the functions of the 'versioned' category are stateful.
+//!
+//! In the internal implementation, each stateful function is implemented based on its corresponding stateless function,
+//! all stateful data has two additional identification dimensions ('branch' and 'version'), somewhat like the logic in GIT.
+//!
+//! Stateless functions do not have the feature of 'version' management, but they have higher performance.
 //!
 
 #![deny(warnings)]
